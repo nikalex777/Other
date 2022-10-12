58c0f7d5b79e432959c7ab30cf2e69475a6e4bbc
nikalex777 <777_nikolaev@mail.ru>
2022-10-12 20:12:13 +0300

    implementation of the bonus when buying

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,7 @@ public class Main {
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
 
-                System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                //System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
 
                 boolean isOnSale = false;
                 for (String saleProduct : productsOnSale) {
@@ -55,10 +55,10 @@ public class Main {
                 }
 
                 if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
                 } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
                     sum += prices[i] * counts[i];
                 }
 
